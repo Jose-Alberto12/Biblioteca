a3ac6d7486647b5357239dc9ea9a972841a60d0e
Jose-Alberto12 <147838227+Jose-Alberto12@users.noreply.github.com>
2023-11-22 08:06:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*document.getElementById('descargar').addEventListener('click', function() {
+    // Crear un enlace temporal
+    var enlace = document.createElement('a');
+    enlace.href = '../romantico/Mi_isla-Elisabet_Benavent.pfd'; // Reemplaza con la ruta del archivo PDF
+    enlace.download = 'Mi_isla.pdf'; // Nombre que tendrá el archivo descargado
+    document.body.appendChild(enlace);
+  
+    // Simular el clic en el enlace para iniciar la descarga
+    enlace.click();
+  
+    // Eliminar el enlace temporal
+    document.body.removeChild(enlace);
+  });
+  */
